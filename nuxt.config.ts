0238f216d427b378c92100f192cfcd6eb8754621
xAsssenov <aks.kirill32@gmail.com>
2024-12-11 22:52:59 -0800

    feat: add favicon icon

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -5,6 +5,7 @@ export default defineNuxtConfig({
             title: 'avb-stroy',
             charset: 'utf-8',
             viewport: 'width=device-width, initial-scale=1',
+            link: [{ rel: 'icon', type: 'image/png', href: '/logo.png' }],
         },
     },
     compatibilityDate: '2024-11-01',
