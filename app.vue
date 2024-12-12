076c59495537193b1b0fc237b46545a4026cd3cb
xAsssenov <aks.kirill32@gmail.com>
2024-12-11 22:35:37 -0800

    refactor: styles in app.vue

--- app.vue
+++ app.vue
@@ -3,3 +3,24 @@
     <NuxtPage />
   </NuxtLayout>
 </template>
+
+<style lang="scss">
+body {
+  font-family: sans-serif;
+  margin: 0;
+  overflow-x: hidden;
+  background: url('/public/background.jpg') no-repeat center center fixed;
+  background-size: cover;
+
+  &::before {
+    content: '';
+    position: fixed;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    background: rgba(0 0 0 / 55%);
+    z-index: 1;
+  }
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -16,26 +16,7 @@ definePageMeta({
 })
 </script>
 
-<style lang="scss">
-body {
-    font-family: sans-serif;
-    margin: 0;
-    overflow-x: hidden;
-    background: url('/public/background.jpg') no-repeat center center fixed;
-    background-size: cover;
-
-    &::before {
-        content: '';
-        position: fixed;
-        top: 0;
-        left: 0;
-        width: 100%;
-        height: 100%;
-        background: rgba(0 0 0 / 55%);
-        z-index: 1;
-    }
-}
-
+<style scoped lang="scss">
 .page {
     position: relative;
     color: white;
