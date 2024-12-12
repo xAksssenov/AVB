1ea2e7a5a667a86519b46e8de15c2739e77d7ffd
xAsssenov <aks.kirill32@gmail.com>
2024-12-11 16:05:06 -0800

    feat: add eslint, prettier, stylelint and test pages

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,27 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2024-11-01',
-  devtools: { enabled: true }
+    app: {
+        head: {
+            title: 'avb-stroy',
+            charset: 'utf-8',
+            viewport: 'width=device-width, initial-scale=1',
+        },
+    },
+    compatibilityDate: '2024-11-01',
+    components: [
+        {
+            path: '~/components',
+            pathPrefix: false,
+        },
+    ],
+    devtools: { enabled: true },
+    modules: ['@nuxtjs/eslint-module', '@nuxtjs/stylelint-module', 'nuxt-svgo'],
+    svgo: {
+        autoImportPath: '~/assets/icons/',
+    },
+    runtimeConfig: {
+        public: {
+            apiBase: 'http://localhost:3000',
+        },
+    },
 })
